d2636daae6b579ecfd7962525dff8481810d26e3
riyandifirman <riyandi.firman08@gmail.com>
2023-03-19 14:21:50 +0700

    finishing simple live data

--- app/src/main/java/com/riyandifirman/mylivedata/MainActivity.kt
+++ app/src/main/java/com/riyandifirman/mylivedata/MainActivity.kt
@@ -2,10 +2,30 @@ package com.riyandifirman.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.riyandifirman.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
